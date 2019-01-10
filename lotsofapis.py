d94fbf869ef044a54c86762b5c9dbfd0ef587cd7
Seon Ricks <skcirs@gmail.com>
2019-01-09 18:07:31 -0800

    refactor: Import User model

--- lotsofapis.py
+++ lotsofapis.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import ApiCategory, Base, Api
+from database_setup import ApiCategory, Base, Api, User
 
 engine = create_engine('sqlite:///api.db')
 # Bind the engine to the metadata of the Base class so that the
