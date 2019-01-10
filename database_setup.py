27ec31c1d9f996709431fc31d1d6d427b7d9b4ad
Seon Ricks <skcirs@gmail.com>
2019-01-09 16:43:48 -0800

    refactor: Set up database for application

---
+++ database_setup.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class ApiCategory(Base):
+    __tablename__ = 'api_category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Api(Base):
+    __tablename__ = 'api'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('ApiCategory.id'))
+    api_category = relationship(ApiCategory)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'category_id': self.category_id
+        }
+
+
+engine = create_engine('sqlite:///api.db')
+
+
+Base.metadata.create_all(engine)
