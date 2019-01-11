9cf56022f9c71540c488e7f58d89de7b5eea0d61
Seon Ricks <skcirs@gmail.com>
2019-01-10 16:05:46 -0800

    bug: Use proper class name

--- lotsofapis.py
+++ lotsofapis.py
@@ -32,14 +32,14 @@ api_category1 = ApiCategory(name="Trivia")
 session.add(api_category1)
 session.commit()
 
-api1 = MenuItem(title="JService.io", description="Serve answers and questions pulled from the televsion show Jeopardy®.",
+api1 = Api(title="JService.io", description="Serve answers and questions pulled from the televsion show Jeopardy.",
                      url="http://jservice.io/", api_category=api_category1)
 
 session.add(api1)
 session.commit()
 
 
-api2 = MenuItem(title="Trivia Nerd", description="Open trivia questions with topics from Star Wars® to the TV Show Firefly.",
+api2 = Api(title="Trivia Nerd", description="Open trivia questions with topics from Star Wars to the TV Show Firefly.",
                      url="https://trivia.propernerd.com/", api_category=api_category1)
 
 session.add(api2)
@@ -52,14 +52,14 @@ api_category2 = ApiCategory(name="Games")
 session.add(api_category2)
 session.commit()
 
-api1 = MenuItem(title="Texas Hold 'Em", description="Easily develop a poker texas holdem game, just design the graphic.",
+api1 = Api(title="Texas Hold 'Em", description="Easily develop a poker texas holdem game, just design the graphic.",
                      url="https://market.mashape.com/vincy/texas-hold-em", api_category=api_category1)
 
 session.add(api1)
 session.commit()
 
 
-api2 = MenuItem(title="Fortnite Tracker", description="An easy way for developers to get information from Fortnite.",
+api2 = Api(title="Fortnite Tracker", description="An easy way for developers to get information from Fortnite.",
                      url="https://fortnitetracker.com/site-api", api_category=api_category1)
 
 session.add(api2)
@@ -73,14 +73,14 @@ api_category3 = ApiCategory(name="Lyrics")
 session.add(api_category3)
 session.commit()
 
-api1 = MenuItem(title="Lyrics.ovh", description="Simple API to retrieve the lyrics of a song.",
+api1 = Api(title="Lyrics.ovh", description="Simple API to retrieve the lyrics of a song.",
                      url="https://lyricsovh.docs.apiary.io/", api_category=api_category1)
 
 session.add(api1)
 session.commit()
 
 
-api2 = MenuItem(title="Musixmatch", description="Over 14 million lyrics in over 50 distinct languages.",
+api2 = Api(title="Musixmatch", description="Over 14 million lyrics in over 50 distinct languages.",
                      url="https://developer.musixmatch.com/", api_category=api_category1)
 
 session.add(api2)
@@ -93,14 +93,14 @@ api_category4 = ApiCategory(name="Humor")
 session.add(api_category4)
 session.commit()
 
-api1 = MenuItem(title="Meme Generator", description="Generate memes with preloaded images plus your copy.",
+api1 = Api(title="Meme Generator", description="Generate memes with preloaded images plus your copy.",
                      url="http://version1.api.memegenerator.net/", api_category=api_category1)
 
 session.add(api1)
 session.commit()
 
 
-api2 = MenuItem(title="Geek Jokes", description="Fetch a random geeky/programming related joke.",
+api2 = Api(title="Geek Jokes", description="Fetch a random geeky/programming related joke.",
                      url="https://github.com/sameerkumar18/geek-joke-api", api_category=api_category1)
 
 session.add(api2)
