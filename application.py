dc68de73871d13719c62be3d35380bd1390898cc
Seon Ricks <skcirs@gmail.com>
2019-01-13 20:05:58 -0800

    Update to PEP8 standards

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask import Flask, render_template, request, redirect
+from flask import jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker, joinedload
 from database_setup import Base, ApiCategory, Api, User
@@ -35,7 +35,6 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
 
 
@@ -91,8 +90,8 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(json.dumps(
+                   'Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -125,7 +124,8 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px; border-radius: '
+    output += '150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;">'
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
@@ -150,7 +150,7 @@ def getUserID(email):
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
-    except:
+    except LookupError:
         return None
 
 
@@ -172,7 +172,8 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response = make_response(json.dumps(
+                   'Failed to revoke token for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -180,9 +181,11 @@ def gdisconnect():
 # JSON API Endpoint
 @app.route('/catalog.json')
 def apiCategoriesJSON():
-    api_categories = session.query(ApiCategory).options(joinedload(ApiCategory.offerings)).all()
-    return jsonify(api_categories=[dict(a.serialize, offerings=[i.serialize for i in a.offerings])
-        for a in api_categories])
+    api_categories = session.query(ApiCategory).options(joinedload(
+                     ApiCategory.offerings)).all()
+    return jsonify(api_categories=[dict(a.serialize,
+                   offerings=[i.serialize for i in a.offerings])
+                   for a in api_categories])
 
 
 # Show all API categories
@@ -191,33 +194,46 @@ def apiCategoriesJSON():
 def showApiCategories():
     api_categories = session.query(ApiCategory).order_by(asc(ApiCategory.id))
     if 'username' not in login_session:
-        return render_template('public-api-categories.html', api_categories=api_categories)
+        return render_template('public-api-categories.html',
+                               api_categories=api_categories)
     else:
-        return render_template('api-categories.html', api_categories=api_categories)
+        return render_template('api-categories.html',
+                               api_categories=api_categories)
 
 
-# # Show an API Category
+# Show an API Category
 @app.route('/catalog/<api_category_slug>/')
 @app.route('/catalog/<api_category_slug>/apis/')
 def showApiCategory(api_category_slug):
-    api_category = session.query(ApiCategory).filter_by(slug=api_category_slug).one()
+    api_category = session.query(ApiCategory).filter_by(
+                   slug=api_category_slug).one()
     apis = session.query(Api).filter_by(category_id=api_category.id).all()
     if 'username' not in login_session:
-        return render_template('public-api-category.html', apis=apis, api_category=api_category, category_id=api_category.id)
+        return render_template('public-api-category.html', apis=apis,
+                               api_category=api_category,
+                               category_id=api_category.id)
     else:
-        return render_template('api-category.html', apis=apis, api_category=api_category, category_id=api_category.id)
+        return render_template('api-category.html', apis=apis,
+                               api_category=api_category,
+                               category_id=api_category.id)
 
 
-# # Show an API
+# Show an API
 @app.route('/catalog/<api_category_slug>/<api_title_slug>/')
 def showApi(api_category_slug, api_title_slug):
     api = session.query(Api).filter_by(slug=api_title_slug).one()
-    api_category = session.query(ApiCategory).filter_by(id=api.category_id).one()
+    api_category = session.query(ApiCategory).filter_by(
+                   id=api.category_id).one()
     creator = getUserInfo(api.user_id)
-    if 'username' not in login_session or creator.id != login_session['user_id']:
-        return render_template('public-api.html', api=api, api_category_name=api_category.name, creator = creator)
+    if 'username' not in login_session
+    or creator.id != login_session['user_id']:
+        return render_template('public-api.html', api=api,
+                               api_category_name=api_category.name,
+                               creator=creator)
     else:
-        return render_template('api.html', api=api, api_category_name=api_category.name, creator = creator)
+        return render_template('api.html', api=api,
+                               api_category_name=api_category.name,
+                               creator=creator)
 
 
 # Create a new API
@@ -226,8 +242,12 @@ def newApi():
     if 'username' not in login_session:
         return redirect('/login')
     if request.method == 'POST':
-        newApi = Api(title=request.form['title'], description=request.form['description'], url=request.form[
-                           'url'], slug=slugify(request.form['title']), category_id=request.form['category'], user_id=login_session['user_id'])
+        newApi = Api(title=request.form['title'],
+                     description=request.form['description'],
+                     url=request.form['url'],
+                     slug=slugify(request.form['title']),
+                     category_id=request.form['category'],
+                     user_id=login_session['user_id'])
         session.add(newApi)
         session.commit()
         flash('%s Successfully Created' % (newApi.title))
@@ -236,14 +256,17 @@ def newApi():
         return render_template('newapi.html')
 
 
-# # Edit an API
+# Edit an API
 @app.route('/catalog/<api_title_slug>/edit/', methods=['GET', 'POST'])
 def editApi(api_title_slug):
     if 'username' not in login_session:
         return redirect('/login')
     editedApi = session.query(Api).filter_by(slug=api_title_slug).one()
     if login_session['user_id'] != editedApi.user_id:
-        return "<script>function myFunction() {alert('You are not authorized to edit this API. Please create your own API in order to have editing privileges.');}</script><body onload='myFunction()'>"
+        return "<script>function myFunction() {alert('You are not " +
+        "authorized to edit this API. Please create your own " +
+        "API in order to have editing privileges.');}" +
+        "</script><body onload='myFunction()'>"
     if request.method == 'POST':
         if request.form['title']:
             editedApi.title = request.form['title']
@@ -269,7 +292,10 @@ def deleteApi(api_title_slug):
         return redirect('/login')
     apiToDelete = session.query(Api).filter_by(slug=api_title_slug).one()
     if login_session['user_id'] != apiToDelete.user_id:
-        return "<script>function myFunction() {alert('You are not authorized to delete this API. Please create your own API in order to have delete privileges.');}</script><body onload='myFunction()'>"
+        return "<script>function myFunction() {alert('You are not " +
+        "authorized to delete this API. Please create your own API " +
+        "in order to have delete privileges.');}</script>" +
+        "<body onload='myFunction()'>"
     if request.method == 'POST':
         session.delete(apiToDelete)
         session.commit()
@@ -279,7 +305,7 @@ def deleteApi(api_title_slug):
         return render_template('deleteapi.html', api=apiToDelete)
 
 
-# Disconnect based on provider
+# Disconnect based on provider, currently supporting Google
 @app.route('/disconnect')
 def disconnect():
     if 'provider' in login_session:
@@ -302,4 +328,4 @@ def disconnect():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000, threaded = False)
+    app.run(host='0.0.0.0', port=5000, threaded=False)
